933f208e85dd9f6ab3d985ea5c7cc4d7e6743cd3
lfcypo <lfcypo@gmail.com>
2025-08-04 23:47:46 +0800

    perf: 替换为跨平台磁盘空间检测

--- src/space.rs
+++ src/space.rs
@@ -1,36 +1,7 @@
-#[cfg(not(target_os = "macos"))]
-use std::path::Path;
-#[cfg(not(target_os = "macos"))]
-use sysinfo::{System, DiskExt, SystemExt};
+use fs2::available_space;
 
-#[cfg(target_os = "macos")]
-use std::ffi::CString;
-#[cfg(target_os = "macos")]
-use libc::{statvfs};
-
-
-#[cfg(not(target_os = "macos"))]
-pub fn check_free_space(target_path: &str, size: &u64) -> (bool, u64) {
-    None // TODO Other OS support.
-}
-
-#[cfg(target_os = "macos")]
 pub fn check_free_space(target_path: &str, size: &u64) -> Option<u64> {
-    let c_path = match CString::new(target_path) {
-        Ok(cstr) => cstr,
-        Err(_) => {
-            return None;
-        }
-    };
-
-    let mut stats = unsafe { std::mem::zeroed() };
-
-    let result = unsafe { statvfs(c_path.as_ptr(), &mut stats) };
-    if result != 0 {
-        return None;
-    }
-
-    let free_space = &(stats.f_bavail as u64 * stats.f_frsize as u64);
+    let free_space = &available_space(target_path).unwrap();
 
     if size <= free_space {
         None

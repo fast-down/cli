7f76f60907ebff36b827d6b80287055a0b70c7a4
lfcypo <lfcypo@gmail.com>
2025-08-04 17:22:14 +0800

    feat: 时间格式化添加天数显示

--- crates/cli/src/fmt/time.rs
+++ crates/cli/src/fmt/time.rs
@@ -1,8 +1,25 @@
+const ONE_SECOND: u64 = 1;
+const ONE_MINUTE: u64 = ONE_SECOND * 60;
+const ONE_HOUR: u64 = ONE_MINUTE * 60;
+const ONE_DAY: u64 = ONE_HOUR * 24;
+
+
 pub fn format_time(time: u64) -> String {
-    let seconds = time % 60;
-    let minutes = (time / 60) % 60;
-    let hours = time / 3600;
-    format!("{hours:02}:{minutes:02}:{seconds:02}")
+    if time < ONE_DAY {
+        let seconds = time % ONE_MINUTE;
+        let minutes = (time / ONE_MINUTE) % ONE_MINUTE;
+        let hours = time / ONE_HOUR;
+        return format!("{hours:02}:{minutes:02}:{seconds:02}");
+    }
+
+    let remainder = time % ONE_DAY;
+
+    let days = time / ONE_DAY;
+    let seconds = remainder % ONE_MINUTE;
+    let minutes = (remainder / ONE_MINUTE) % ONE_MINUTE;
+    let hours = remainder / ONE_HOUR;
+
+    format!("{days}d {hours:02}:{minutes:02}:{seconds:02}")
 }
 
 #[cfg(test)]
@@ -18,5 +35,9 @@ mod tests {
         assert_eq!(format_time(3600), "01:00:00");
         assert_eq!(format_time(3661), "01:01:01");
         assert_eq!(format_time(86399), "23:59:59");
+        assert_eq!(format_time(86400), "1d 00:00:00");
+        assert_eq!(format_time(86401), "1d 00:00:01");
+        assert_eq!(format_time(95400), "1d 02:30:00");
+        assert_eq!(format_time(8726399), "100d 23:59:59");
     }
 }

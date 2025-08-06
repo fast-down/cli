fa20b8e475ed12dc6301ba067e89bf4fb46ea4c8
share121 <2854631158@qq.com>
2025-08-07 07:11:45 +0800

    chore: 整理

--- src/clean.rs
+++ src/clean.rs
@@ -4,6 +4,6 @@ use color_eyre::Result;
 pub async fn clean() -> Result<()> {
     let db = Database::new().await?;
     let len = db.clean_finished().await?;
-    eprintln!("已清理 {len} 行链接");
+    eprintln!("{}", t!("msg.clean", count = len));
     Ok(())
 }
